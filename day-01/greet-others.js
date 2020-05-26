59a2904dec3d92eacce3980ad6621fae0328f60f
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-05-26 18:06:21 +0200

    deleted clutter

--- day-01/greet-others.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-// Greet 3 of your classmates with this program, in three separate lines like:
-//
-// Hello, Esther!
-console.log("Hello, Esther!");
-// Hello, Mary!
-console.log("Hello, Mary!");
-// Hello, Joe!
-console.log("Hello, Joe!");
